c8ac02b5b2883c48916641aa094c671a24f21a12
Marco Ramirez <gonzalezramirez.marco@gmail.com>
2020-07-03 17:18:36 -0500

    Correccion de errores
    Correccion de errores y direccionamiento por el json

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InformacionService } from '../../services/informacion.service';
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +11,8 @@ export class FooterComponent {
 
 anio:number = new Date().getFullYear();
 
+constructor ( public _is:InformacionService){
 
+}
 
 }
